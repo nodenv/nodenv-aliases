6d0bb5015c8eda6896c9c2b7bf90016a05c7068b
Jason Karns <jason@karns.name>
2015-11-14 15:24:06 -0500

    shellcheck fix
    substring removal via string manipulation instead of sed

--- etc/nodenv.d/install/autoalias.bash
+++ etc/nodenv.d/install/autoalias.bash
@@ -1,15 +1,11 @@
 after_install autoalias
 
-strip_patch() {
-  sed -E -e 's/\.[0-9]+$//'
-}
-
 autoalias() {
   if [ "$STATUS" = 0 ]; then
     # this isn't currently used by nodenv
     case "$VERSION_NAME" in
       *[0-9]-*-*)
-        local patch="$(echo $VERSION_NAME|sed -e 's/-[^-]*$//')"
+        local patch="${VERSION_NAME%-*}"
         if [ ! -e "$NODENV_ROOT/$patch" ]; then
           # nodenv alias 1.9.3-p194 1.9.3-p194-perf
           nodenv alias "$patch" "$VERSION_NAME"
@@ -18,7 +14,7 @@ autoalias() {
     esac
     case "$VERSION_NAME" in
       *.*.*)
-        nodenv alias "$(echo "$VERSION_NAME" | strip_patch)" --auto 2>/dev/null || true
+        nodenv alias "${VERSION_NAME%.*}" --auto 2>/dev/null || true
         ;;
     esac
   fi
