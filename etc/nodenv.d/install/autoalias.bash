4ae617c3b5fa5171bad0a1379ea0114af57ebc5f
Jason Karns <jason@karns.name>
2015-11-17 08:32:27 -0500

    remove rbenv-only code branch

--- etc/nodenv.d/install/autoalias.bash
+++ etc/nodenv.d/install/autoalias.bash
@@ -2,16 +2,6 @@ after_install autoalias
 
 autoalias() {
   if [ "$STATUS" = 0 ]; then
-    # this isn't currently used by nodenv
-    case "$VERSION_NAME" in
-      *[0-9]-*-*)
-        local patch="${VERSION_NAME%-*}"
-        if [ ! -e "$NODENV_ROOT/$patch" ]; then
-          # nodenv alias 1.9.3-p194 1.9.3-p194-perf
-          nodenv alias "$patch" "$VERSION_NAME"
-        fi
-        ;;
-    esac
     case "$VERSION_NAME" in
       *.*.*)
         nodenv alias "${VERSION_NAME%.*}" --auto 2>/dev/null || true
