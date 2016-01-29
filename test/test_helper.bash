5e6b313dbd392c02ef75d51821f78d14d159f487
Jason Karns <jason@karns.name>
2016-01-28 22:47:09 -0500

    Remove unnecessary env vars

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -10,16 +10,12 @@ NODENV_TEST_DIR="${BATS_TMPDIR}/nodenv"
 # guard against executing this block twice due to bats internals
 if [ "$NODENV_ROOT" != "${NODENV_TEST_DIR}/root" ]; then
   export NODENV_ROOT="${NODENV_TEST_DIR}/root"
-  export HOME="${NODENV_TEST_DIR}/home"
-
 
   PATH=/usr/bin:/bin:/usr/sbin:/sbin
-  PATH="${NODENV_TEST_DIR}/bin:$PATH"
   PATH="${BATS_TEST_DIRNAME}/bin:$PATH"
   PATH="${BATS_TEST_DIRNAME}/../bin:$PATH"
   PATH="${BATS_TEST_DIRNAME}/../nodenv/libexec:$PATH"
   PATH="${BATS_TEST_DIRNAME}/../nodenv/test/libexec:$PATH"
-  PATH="${NODENV_ROOT}/shims:$PATH"
   export PATH
 fi
 
@@ -31,7 +27,7 @@ teardown() {
 create_versions() {
   for v; do
     #echo "Created version: $d"
-    d="$NODENV_TEST_DIR/root/versions/$v"
+    d="$NODENV_ROOT/versions/$v"
     mkdir -p "$d/bin"
     echo $v > "$d/RELEASE.txt"
     ln -nfs /bin/echo "$d/bin/node"
@@ -60,17 +56,5 @@ assert_alias_version() {
 assert_alias_missing() {
   if [ -f "$NODENV_ROOT/versions/$1/RELEASE.txt" ]; then
     assert_equal "no-version" "$(cat "$NODENV_ROOT/versions/$1/RELEASE.txt" 2>&1)"
-  fi
-}
-
-assert_line_starts_with() {
-  if [ "$1" -ge 0 ] 2>/dev/null; then
-    assert_equal "$2" "${lines[$1]}"
-  else
-    local line
-    for line in "${lines[@]}"; do
-      if [ -n "${line#${1}}" ]; then return 0; fi
-    done
-    flunk "expected line \`$1'"
   fi
 }
